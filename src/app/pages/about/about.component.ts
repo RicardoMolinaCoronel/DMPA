9fcec64ed372b52d851a824b20af38d7df7db1a4
Cliente CONTROL-PC <CltControl@GGIECLUGWRK169.espol.edu.ec>
2023-08-02 13:45:28 -0500

    guia19

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  description: string = 'Descripción del proyecto 7'; /* Cambie el texto con la descripción de su proyecto */
+}
