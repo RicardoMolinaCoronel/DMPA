aab0b9aea863295b4878e06e4f4758b792f77e04
RicardoMolinaCoronel <84423995+RicardoMolinaCoronel@users.noreply.github.com>
2023-09-04 13:42:17 -0500

    Update proveedor.service.ts

--- src/app/providers/proveedor.service.ts
+++ src/app/providers/proveedor.service.ts
@@ -8,8 +8,8 @@ export class ProveedorService {
   headers = new HttpHeaders({
     'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJhZG1pbiIsInJvbCI6ImFkbWluIiwiaWF0IjoxNjkzNzgxMzQ4fQ.-lgNy0JJ-6ISUnRlMekXpMwoG1X-si_yE-eP9Zn9zVU`
   });
-	private URL: string = 'http://localhost:3000/rest/cliente/findAll/json';
-  private URL2: string='http://localhost:3000/rest/pedido/findAllPedidosByClientId/';
+	private URL: string = 'https://backdmpa-production.up.railway.app/rest/cliente/findAll/json';
+  private URL2: string='https://backdmpa-production.up.railway.app/rest/pedido/findAllPedidosByClientId/';
   constructor(private http:HttpClient) { }
 getResponse() {
           return this.http.get(this.URL,{ headers: this.headers });
